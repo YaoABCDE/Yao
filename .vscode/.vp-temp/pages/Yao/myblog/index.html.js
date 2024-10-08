df84b11a33441b739c06554dc974c8420cde4ab2
YAO <1964877312@qq.com>
2024-10-09 01:28:22 +0800

    28

---
+++ .vscode/.vp-temp/pages/Yao/myblog/index.html.js
@@ -0,0 +1,16 @@
+import comp from "D:/node/Yaoweb5/.vscode/.vp-temp/pages/Yao/myblog/index.html.vue"
+const data = JSON.parse("{\"path\":\"/Yao/myblog/\",\"title\":\"收藏\",\"lang\":\"zh-CN\",\"frontmatter\":{\"icon\":\"favoritea\",\"pageInfo\":[\"Author\",\"PageView\"],\"date\":\"1993-02-13T00:00:00.000Z\",\"index\":false,\"permalink\":\"/Yao/myblog/\",\"description\":\"收藏 有些记忆，弥足珍贵\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://yaoabcde.github.io/Yao/Yao/Yao/myblog/\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"YAO\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"收藏\"}],[\"meta\",{\"property\":\"og:description\",\"content\":\"收藏 有些记忆，弥足珍贵\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"article\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-CN\"}],[\"meta\",{\"property\":\"og:updated_time\",\"content\":\"2024-10-08T17:24:05.000Z\"}],[\"meta\",{\"property\":\"article:published_time\",\"content\":\"1993-02-13T00:00:00.000Z\"}],[\"meta\",{\"property\":\"article:modified_time\",\"content\":\"2024-10-08T17:24:05.000Z\"}],[\"script\",{\"type\":\"application/ld+json\"},\"{\\\"@context\\\":\\\"https://schema.org\\\",\\\"@type\\\":\\\"Article\\\",\\\"headline\\\":\\\"收藏\\\",\\\"image\\\":[\\\"\\\"],\\\"datePublished\\\":\\\"1993-02-13T00:00:00.000Z\\\",\\\"dateModified\\\":\\\"2024-10-08T17:24:05.000Z\\\",\\\"author\\\":[{\\\"@type\\\":\\\"Person\\\",\\\"name\\\":\\\"Yao\\\",\\\"url\\\":\\\"https://baidu.com\\\",\\\"email\\\":\\\"1964877312@qq.com\\\"}]}\"]]},\"headers\":[],\"git\":{\"createdTime\":1728311364000,\"updatedTime\":1728408245000,\"contributors\":[{\"name\":\"YAO\",\"email\":\"1964877312@qq.com\",\"commits\":1}]},\"readingTime\":{\"minutes\":0.09,\"words\":28},\"filePathRelative\":\"Yao/myblog/README.md\",\"localizedDate\":\"1993年2月13日\",\"excerpt\":\"\\n<div class=\\\"hint-container info\\\">\\n<p class=\\\"hint-container-title\\\">有些记忆，弥足珍贵</p>\\n</div>\\n<hr>\\n\",\"autoDesc\":true}")
+export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
---
+++ .vscode/.vp-temp/pages/Yao/myblog/me.html.js
@@ -0,0 +1,16 @@
+import comp from "D:/node/Yaoweb5/.vscode/.vp-temp/pages/Yao/myblog/me.html.vue"
+const data = JSON.parse("{\"path\":\"/Yao/myblog/me.html\",\"title\":\"关于我 测试\",\"lang\":\"zh-CN\",\"frontmatter\":{\"icon\":\"selfinfo\",\"star\":3,\"order\":3,\"permalink\":\"/Yao/myblog/me.html\",\"description\":\"关于我 测试\",\"head\":[[\"meta\",{\"property\":\"og:url\",\"content\":\"https://yaoabcde.github.io/Yao/Yao/Yao/myblog/me.html\"}],[\"meta\",{\"property\":\"og:site_name\",\"content\":\"YAO\"}],[\"meta\",{\"property\":\"og:title\",\"content\":\"关于我 测试\"}],[\"meta\",{\"property\":\"og:description\",\"content\":\"关于我 测试\"}],[\"meta\",{\"property\":\"og:type\",\"content\":\"article\"}],[\"meta\",{\"property\":\"og:locale\",\"content\":\"zh-CN\"}],[\"meta\",{\"property\":\"og:updated_time\",\"content\":\"2024-10-08T17:24:05.000Z\"}],[\"meta\",{\"property\":\"article:modified_time\",\"content\":\"2024-10-08T17:24:05.000Z\"}],[\"script\",{\"type\":\"application/ld+json\"},\"{\\\"@context\\\":\\\"https://schema.org\\\",\\\"@type\\\":\\\"Article\\\",\\\"headline\\\":\\\"关于我 测试\\\",\\\"image\\\":[\\\"\\\"],\\\"dateModified\\\":\\\"2024-10-08T17:24:05.000Z\\\",\\\"author\\\":[{\\\"@type\\\":\\\"Person\\\",\\\"name\\\":\\\"Yao\\\",\\\"url\\\":\\\"https://baidu.com\\\",\\\"email\\\":\\\"1964877312@qq.com\\\"}]}\"]]},\"headers\":[],\"git\":{\"createdTime\":1728408245000,\"updatedTime\":1728408245000,\"contributors\":[{\"name\":\"YAO\",\"email\":\"1964877312@qq.com\",\"commits\":1}]},\"readingTime\":{\"minutes\":0.04,\"words\":13},\"filePathRelative\":\"Yao/myblog/姚的介绍.md\",\"localizedDate\":\"2024年10月9日\",\"excerpt\":\"\\n\",\"autoDesc\":true}")
+export { comp, data }
+
+if (import.meta.webpackHot) {
+  import.meta.webpackHot.accept()
+  if (__VUE_HMR_RUNTIME__.updatePageData) {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  }
+}
+
+if (import.meta.hot) {
+  import.meta.hot.accept(({ data }) => {
+    __VUE_HMR_RUNTIME__.updatePageData(data)
+  })
+}
